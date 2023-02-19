23c4e67bd40f2c48c730610cb0ff0a4c3439c8b3
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-02-19 12:02:01 +0100

    Make basic design of the main page (WIP)

--- health-pad/src/App.svelte
+++ health-pad/src/App.svelte
@@ -1,39 +1,21 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import Datatables from './lib/Datatables.svelte'
+  import Navigation from './lib/Navigation.svelte';
+  import { Container, Row, Col, Styles } from 'sveltestrap';
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
+<Styles />
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
-  </div>
-
-
-</main>
-
-<style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
-</style>
+<Container>
+  <Row>
+    <Col>
+      <Navigation />
+    </Col>
+  </Row>
+  
+  <Row>
+    <Col>
+      <Datatables/>
+    </Col>
+  </Row>
+</Container>
---
+++ health-pad/src/lib/Datatables.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { TabContent, TabPane } from 'sveltestrap';
+</script>
+
+<TabContent>
+  <TabPane tabId="appointments" tab="Appointments" active>
+    <h2>Appointments</h2>
+    <img
+      alt="Alpha Flight"
+      src="https://upload.wikimedia.org/wikipedia/en/4/49/Alpha_Flight_cast_picture_%28John_Byrne_era%29.gif"
+    />
+  </TabPane>
+  <TabPane tabId="patients" tab="Patients">
+    <h2>Patients</h2>
+    <img
+      alt="Johnny Bravo"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Johnny_Bravo_series_logo.png/320px-Johnny_Bravo_series_logo.png"
+    />
+  </TabPane>
+</TabContent>
